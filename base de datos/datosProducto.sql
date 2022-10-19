f090466ead8e7dd41e7a9b5a0f4e945d48a98a50
blanca-web <nag310798@gmail.com>
2022-10-19 02:24:19 -0500

    base de datos

---
+++ base de datos/datosProducto.sql
@@ -0,0 +1,9 @@
+select * from producto
+
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (lapiz,10,2022-05-03,Papelera SA de CV,oficina);
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (goma,15,2022-05-05,Papelera SA de CV,oficina);
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (sacapuntas,12,2022-05-05,Papelera SA de CV,oficina);
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (resistol,5,2022-09-20,Papelera SA de CV,oficina);
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (agua,7,2022-07-07,Tiendita de la esquina,viaticos);
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (papitas,16,2022-10-01,Tiendita de la esquina,viaticos);
+insert into producto(id,nombre,cantidad,fecha_actualizacion,idProveedor,idCategoria) values (cable,1,2022-06-14,Compania de Electronica,electronica);
